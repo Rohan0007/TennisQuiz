1172a39c52885050a144ff4893840aa19772ad20
Rohan0007 <rohanwadhawan007@gmail.com>
2017-10-25 16:09:59 -0700

    Unwanted code removal

--- app/src/main/java/com/example/android/tennisquiz/MainActivity.java
+++ app/src/main/java/com/example/android/tennisquiz/MainActivity.java
@@ -1,17 +1,14 @@
 package com.example.android.tennisquiz;
 
-import android.graphics.Typeface;
-import android.support.v4.content.res.ConfigurationHelper;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import static android.R.attr.button;
 import static com.example.android.tennisquiz.R.id.a1;
 import static com.example.android.tennisquiz.R.id.a2;
 import static com.example.android.tennisquiz.R.id.a3;
